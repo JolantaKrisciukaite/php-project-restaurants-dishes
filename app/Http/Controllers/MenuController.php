d5d0d7ebffcf5c05fa4a13d1ec21cab2519c91ab
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-29 20:56:27 +0300

    a

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -24,7 +24,7 @@ class MenuController extends Controller
      */
     public function create()
     {
-        //
+        return view('menu.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'menus'], function(){
+    Route::get('', [MenuController::class, 'index'])->name('menu.index');
+    Route::get('create', [MenuController::class, 'create'])->name('menu.create');
+    Route::post('store', [MenuController::class, 'store'])->name('menu.store');
+    Route::get('edit/{menu}', [MenuController::class, 'edit'])->name('menu.edit');
+    Route::post('update/{menu}', [MenuController::class, 'update'])->name('menu.update');
+    Route::post('delete/{menu}', [MenuController::class, 'destroy'])->name('menu.destroy');
+    Route::get('show/{menu}', [MenuController::class, 'show'])->name('menu.show');
+ });
+ 
