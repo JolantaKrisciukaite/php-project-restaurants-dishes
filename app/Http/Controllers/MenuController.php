4852a7dcbcdc4515a97d396268fe4eeb53f3ec2c
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-30 13:00:51 +0300

    a

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Menu;
 use Illuminate\Http\Request;
+use Validator;
 
 class MenuController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
     /**
      * Display a listing of the resource.
      *
@@ -37,6 +43,25 @@ class MenuController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(),
+        [
+            'menu_title' => ['required', 'min:3', 'max:200', 'alpha'],
+            'menu_price' => ['required', 'decimal:6,2'],
+            'menu_weight' => ['required'],
+            'menu_meat' => ['required'],
+            'menu_about' => ['required']
+        ],
+        );
+
+        if ($validator->fails()) {
+            $request->flash();
+            return redirect()->back()->withErrors($validator);
+        }
+
+        elseif ($request->runs < $request->wins){
+            return redirect()->back()->with('info_message', 'Menu wins couldn\'t\ be greater then menu runs.');
+        }
+
         $menu = new Menu;
         $menu->title = $request->menu_title;
         $menu->price = $request->menu_price;
