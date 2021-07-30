339f7e65fc6a9a073e03b7a9647b2eab8466f41a
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-30 11:38:50 +0300

    a

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,14 +1,32 @@
-@foreach ($menus as $menu)
-  <a href="{{route('menu.edit',[$menu])}}">
-    {{$menu->title}} 
-    {{$menu->price}} 
-    {{$menu->weight}}
-    {{$menu->meat}} 
-    {{$menu->about}}
- </a>
-  <form method="POST" action="{{route('menu.destroy', $menu)}}">
-   @csrf
-   <button type="submit">DELETE</button>
-  </form>
-  <br>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-header">Menus</div>
+
+               <div class="card-body">
+                @foreach ($menus as $menu)
+                <div class="index">
+                  {{$menu->title}} 
+                  {{$menu->price}} 
+                  {{$menu->weight}}
+                  {{$menu->meat}} 
+                  {{$menu->about}}
+                </div>
+                <form method="POST" action="{{route('menu.destroy', $menu)}}">
+                    <a href="{{route('menu.edit',[$menu])}}" class="editButton">Edit</a>
+                 @csrf
+                 <button class="deleteButton" type="submit">Delete</button>
+                </form>
+                <br>
+              @endforeach
+              
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+@endsection
