906c12f173e54b9c5081d3ac9681205573859bfd
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-30 21:31:36 +0300

    a

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -5,61 +5,59 @@
         <div class="row justify-content-center">
             <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
                 <div class="card">
-                    <div class="titleMenus">
+                    <div class="card-header">
                  
-                      <h3 class="title">Restaurants</h3>
-                      <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
-                          <fieldset>
-                              <legend>Sort by</legend>
-                              <div>
-                                  <label>Type</label>
-                                  <input type="radio" name="sort_by" value="type" @if ('type' == $sort) checked @endif>
-                                  <label>Size</label>
-                                  <input type="radio" name="sort_by" value="size" @if ('size' == $sort) checked @endif>
-                              </div>
-                          </fieldset>
+                      <h3 class="titleRestaurants">Restaurants</h3>
 
+                      <div>
+                        <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
                             <fieldset>
-                                <legend>Direction</legend>
+                                <legend>Sort by</legend>
                                 <div>
-                                    <label>Asc</label>
-                                    <input type="radio" name="dir" value="asc" @if ('asc' == $dir) checked @endif>
-                                    <label>Dsc</label>
-                                    <input type="radio" name="dir" value="desc" @if ('desc' == $dir) checked @endif>
+                                    <label>Type</label>
+                                    <input type="radio" name="sort_by" value="type" @if ('type' == $sort) checked @endif>
+                                    <label>Size</label>
+                                    <input type="radio" name="sort_by" value="size" @if ('size' == $sort) checked @endif>
                                 </div>
                             </fieldset>
-                            <button class="addButtonCreate" type="submit">Sort</button>
-                            <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
-                        </form>
 
-                        <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
-                            <fieldset class="fieldset">
-                                <legend>Filter by</legend>
-                                <select class="index" name="menu_id"><br>
-                                    @foreach ($menus as $menu)
-                                        <option value="{{ $menu->id }}" @if($defaultMenu == $menu->id) selected @endif>
-                                            {{$menu->title}} 
-                                            {{$menu->price}} 
-                                            {{$menu->weight}}
-                                            {{$menu->meat}} 
-                                            {{$menu->about}}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </fieldset>
-                            <button class="addButtonCreate" type="submit">Filter</button>
-                            <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
-                        </form>
+                                <fieldset class="direction">
+                                    <legend>Direction</legend>
+                                    <div>
+                                        <label>Asc</label>
+                                        <input type="radio" name="dir" value="asc" @if ('asc' == $dir) checked @endif>
+                                        <label>Dsc</label>
+                                        <input type="radio" name="dir" value="desc" @if ('desc' == $dir) checked @endif>
+                                    </div>
+                                </fieldset>
+                                <button class="addButtonCreate" type="submit">Sort</button>
+                                <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
+                            </form>
 
-                        <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
-                            <fieldset class="fieldset">
-                                <legend>Search by</legend>
-                                <input placeholder="Serach by type" type="text" class="index" name="s" value="{{$s}}">
-                            </fieldset>
-                            <button class="addButtonCreate" type="submit" name="do_search" value="1">Type</button>
-                            <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
-                        </form>
+                            <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
+                                <fieldset class="filterBy">
+                                    <legend>Filter by</legend>
+                                    <select class="index" name="menu_id"><br>
+                                        @foreach ($menus as $menu)
+                                            <option value="{{ $menu->id }}" @if($defaultMenu == $menu->id) selected @endif>
+                                                {{$menu->title}} 
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </fieldset>
+                                <button class="addButtonCreate" type="submit">Filter</button>
+                                <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
+                            </form>
 
+                            <form action="{{ route('restaurant.index') }}" method="get" class="sort-form">
+                                <fieldset class="searchBy">
+                                    <legend>Search by</legend>
+                                    <input placeholder="Serach by type" type="text" class="index" name="s" value="{{$s}}">
+                                </fieldset>
+                                <button class="addButtonCreate" type="submit" name="do_search" value="1">Type</button>
+                                <a href="{{ route('restaurant.index') }}" class="aButton">Clear</button></a>
+                            </form>
+                        </div>
                     </div>
 
                 <div class="pager-links">
