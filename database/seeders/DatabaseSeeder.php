5399f8fea4990ef91e4a9c54b451fc3d7c035905
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-30 18:39:21 +0300

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+// use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Jolanta',
+            'email' => 'jkrisciukaite@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Raudona',
+            'email' => 'raudona.zeme@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+
+        foreach(range(1, 20) as $_) {
+            DB::table('horses')->insert([
+                'name' => $faker->firstname,
+                'runs' => rand(1, 10000),
+                'wins' => rand(1, 10000),
+                'about' => $faker->realText(300, 5)
+            ]);
+        }
+        
+        foreach(range(1, 200) as $_) {
+            DB::table('betters')->insert([
+                'name' => $faker->firstname,
+                'surname' => $faker->lastname,
+                'bet' => rand(1, 50000),
+                'horse_id' => rand(1, 20)
+            ]);
+        }
     }
 }
