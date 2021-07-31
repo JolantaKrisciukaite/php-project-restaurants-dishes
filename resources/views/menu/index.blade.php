066ca5646ae3a5f9bd14fe2557cba4440039f34a
Jolanta Krisciukaite <79446323+JolantaKrisciukaite@users.noreply.github.com>
2021-07-31 16:21:35 +0300

    a

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,62 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-   <div class="row justify-content-center">
-       <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
-           <div class="card">
-            <div class="card-header">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="titleRestaurants">Menus</h3>
 
-               <h3 class="titleRestaurants">Menus</h3>
+                        <div>
 
-               <div>
+                            <form action="{{ route('menu.index') }}" method="get" class="sort-form">
+                                <fieldset>
+                                    <legend>Sort by</legend>
+                                    <div>
+                                        <label>Price</label>
+                                        <input type="radio" name="sort_by" value="price" @if ('price' == $sort) checked @endif>
+                                        <label>Title</label>
+                                        <input type="radio" name="sort_by" value="title" @if ('title' == $sort) checked @endif>
+                                    </div>
+                                </fieldset>
 
-                    <form action="{{ route('menu.index') }}" method="get" class="sort-form">
-                        <fieldset>
-                            <legend>Sort by</legend>
-                            <div>
-                                <label>Price</label>
-                                <input type="radio" name="sort_by" value="price" @if ('price' == $sort) checked @endif>
-                                <label>Title</label>
-                                <input type="radio" name="sort_by" value="title" @if ('title' == $sort) checked @endif>
-                            </div>
-                        </fieldset>
+                                <fieldset class="direction">
+                                    <legend>Direction</legend>
+                                    <div>
+                                        <label>Asc</label>
+                                        <input type="radio" name="dir" value="asc" @if ('asc' == $dir) checked @endif>
+                                        <label>Dsc</label>
+                                        <input type="radio" name="dir" value="desc" @if ('desc' == $dir) checked @endif>
+                                    </div>
+                                </fieldset>
+                                <button class="addButtonCreate" type="submit">Sort</button>
+                                <a href="{{ route('menu.index') }}" class="aButton">Clear</button></a>
+                            </form>
 
-                            <fieldset class="direction">
-                                <legend>Direction</legend>
-                                <div>
-                                    <label>Asc</label>
-                                    <input type="radio" name="dir" value="asc" @if ('asc' == $dir) checked @endif>
-                                    <label>Dsc</label>
-                                    <input type="radio" name="dir" value="desc" @if ('desc' == $dir) checked @endif>
-                                </div>
-                            </fieldset>
-                            <button class="addButtonCreate" type="submit">Sort</button>
-                            <a href="{{ route('menu.index') }}" class="aButton">Clear</button></a>
-                        </form>
+                        </div>
+                    </div>
+                    <div class="card-body">
 
+                        @foreach ($menus as $menu)
+                            <div class="index">Meal title: {{ $menu->title }}</div>
+                            <div class="index">Price: {{ $menu->price }} €</div>
+                            <div class="index">Portion weight: {{ $menu->weight }} g</div>
+                            <div class="index">Amount of meat: {{ $menu->meat }} g</div>
+                            <div class="index">About: {!! $menu->about !!}</div>
+
+                            <form method="POST" action="{{ route('menu.destroy', $menu) }}">
+                                <a href="{{ route('menu.edit', [$menu]) }}" class="editButton">Edit</a>
+                                @csrf
+                                <button class="deleteButton" type="submit">Delete</button>
+                            </form>
+                            <br>
+                        @endforeach
+
+                    </div>
                 </div>
             </div>
-               <div class="card-body">
-
-                    @foreach ($menus as $menu)
-                    <div class="index">Meal title: {{$menu->title}}</div>
-                    <div class="index">Price: {{$menu->price}} €</div>
-                    <div class="index">Portion weight: {{$menu->weight}} g</div>
-                    <div class="index">Amount of meat: {{$menu->meat}} g</div>
-                    <div class="index">About: {!!$menu->about!!}</div>
-               
-                    <form method="POST" action="{{route('menu.destroy', $menu)}}">
-                        <a href="{{route('menu.edit', [$menu])}}" class="editButton">Edit</a>
-                    @csrf
-                    <button class="deleteButton" type="submit">Delete</button>
-                    </form>
-                    <br>
-                @endforeach
-              
-               </div>
-           
-       </div>
-   </div>
-</div>
-@endsection
+        </div>
+    @endsection
